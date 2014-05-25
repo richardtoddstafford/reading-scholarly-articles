6c4212f4122c30b3887c881a9788cea9a707ed39
Jean Michel Rouly <michel.rouly@gmail.com>
2014-05-25 11:58:26 -0400

    Added abstract popover to engineering article.

--- webroot/content/assets/engineering/annotations.js
+++ webroot/content/assets/engineering/annotations.js
@@ -15,3 +15,12 @@ $("#authors").popover({
   title:   "Authors",
   content: "Authors and their credentials will be provided in a scholarly article. Credentials may appear with the authors' names, as in this example, or they may appear as a footnote or an endnote to the article. The authors' credentials are provided to establish the authority of the authors, and also to provide a point of contact for the research presented in the article. For this reason, authors' e-mail addresses are usually provided in recent articles.",
 });
+
+$("#abstract").popover({
+  toggle:    "popover",
+  trigger:   "click",
+  placement: "auto",
+
+  title:   "Abstract",
+  content: "The abstract is a brief summary of the contents of the article, usually under 250 words. It will contain a description of the problem and problem setting; an outline of the study, experiment, or argument; and a summary of the conclusions or findings. It is provided so that readers examining the article can decide quickly whether the article meets their needs.",
+});
