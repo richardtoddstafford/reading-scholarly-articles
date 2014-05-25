5c45fe420fc0495f4781718e269ca3b8348c7725
Jean Michel Rouly <michel.rouly@gmail.com>
2014-05-25 11:55:19 -0400

    Add a clicked state to triggers

--- webroot/static/js/annotations.js
+++ webroot/static/js/annotations.js
@@ -1,3 +1,5 @@
 $("[rel='annotation']").click(function () {
     $("[rel='annotation']").not(this).popover('hide');
+    $("[rel='annotation']").not(this).removeClass("active");
+    $(this).toggleClass("active");
 });
