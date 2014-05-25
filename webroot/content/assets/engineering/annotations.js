2883050a04205cdb60ac3cc3c1e5391d92b217bb
Jean Michel Rouly <michel.rouly@gmail.com>
2014-05-25 11:29:30 -0400

    Included two new annotations for the engineering paper

---
+++ webroot/content/assets/engineering/annotations.js
@@ -0,0 +1,17 @@
+$("#title").popover({
+  toggle:    "popover",
+  trigger:   "click",
+  placement: "auto",
+
+  title:   "Title",
+  content: "The title of a scholarly article is generally (but not always) an extremely brief summary of the article's contents. It will usually contain technical terms related to the research presented.",
+});
+
+$("#authors").popover({
+  toggle:    "popover",
+  trigger:   "click",
+  placement: "auto",
+
+  title:   "Authors",
+  content: "Authors and their credentials will be provided in a scholarly article. Credentials may appear with the authors' names, as in this example, or they may appear as a footnote or an endnote to the article. The authors' credentials are provided to establish the authority of the authors, and also to provide a point of contact for the research presented in the article. For this reason, authors' e-mail addresses are usually provided in recent articles.",
+});
