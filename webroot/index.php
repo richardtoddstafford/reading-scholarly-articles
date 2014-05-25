d9f833d0f8db6ee5a9db56d1a2f61ae4ab6e58b9
Jean Michel Rouly <michel.rouly@gmail.com>
2014-05-25 09:43:23 -0400

    Actually the GPL not Apache v2

--- webroot/index.php
+++ webroot/index.php
@@ -140,7 +140,7 @@ $path = substr($request_uri['path'], 1);
         <div class="col-sm-12 text-center">
           Built by <a href="http://michel.rouly.net">Michel Rouly</a> for
           <a href="//gmu.edu">George Mason University</a> with
-          <a href="http://www.apache.org/licenses/LICENSE-2.0">some rights
+          <a href="http://www.gnu.org/licenses/gpl-3.0.txt">some rights
           reserved</a>.
         </div>
       </div>
