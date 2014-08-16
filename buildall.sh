0600531d65038cc4c5ca66551ed0b1ab75004df2
Jean Michel Rouly <michel.rouly@gmail.com>
2014-08-16 15:53:29 -0400

    Consolidated build script

--- buildall.sh
+++ buildall.sh
@@ -2,10 +2,24 @@
 
 set -e
 
+# Clean up first
+rm -f webroot/*.html
+mkdir -p tmp
+
 for page in pages/content/*
 do
 
-  id=$(echo $page | sed 's/[^/]*\/[^/]*\///')
-  build-page.sh $id
+  FILENAME=$(echo $page | sed 's/[^/]*\/[^/]*\///')
+  TITLE=$(head -n 1 pages/titles/$FILENAME)
+
+  cat layout/head.html        >  "tmp/$FILENAME.html"
+  cat pages/content/$FILENAME >> "tmp/$FILENAME.html"
+  cat layout/bottom.html      >> "tmp/$FILENAME.html"
+
+  sed -i "s/{{ TITLE }}/$TITLE/" "tmp/$FILENAME.html"
+
+  mv tmp/$FILENAME.html webroot/
 
 done
+
+rmdir tmp
