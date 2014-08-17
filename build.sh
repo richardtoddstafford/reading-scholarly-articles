cd7bca6aebc2cd725b59264c8cece4425c460a29
Jean Michel Rouly <michel.rouly@gmail.com>
2014-08-17 19:11:21 -0400

    Rewrote build script to properly interpret new pages directory structure.

--- build.sh
+++ build.sh
@@ -1,29 +1,107 @@
 #!/bin/bash
 
+
 set -e
 
+
 # Clean up first
+echo "Cleaning build directory."
 clean.sh
 mkdir -p tmp
 mkdir -p webroot
 
-for page in pages/content/*
+
+echo "Copying static files into webroot."
+cp -r static webroot/
+cp -r annotations.json webroot/
+
+echo "Building articles."
+for article in pages/articles/*
 do
 
-  FILENAME=$(echo $page | sed 's/[^/]*\/[^/]*\///')
-  TITLE=$(head -n 1 pages/titles/$FILENAME)
+  echo "Operating on $article."
 
-  cat layout/head.html        >  "tmp/$FILENAME.html"
-  cat pages/content/$FILENAME >> "tmp/$FILENAME.html"
-  cat layout/foot.html        >> "tmp/$FILENAME.html"
+  echo "  Generating asset filenames."
+  ID=$(echo $article | sed 's/[^/]*\/[^/]*\///' )
+  TITLE_FILE="pages/articles/$ID/title"
+  ARTICLE_FILE="pages/articles/$ID/article"
+  OVERVIEW_FILE="pages/articles/$ID/overview"
+  COPYRIGHT_FILE="pages/articles/$ID/copyright"
 
-  sed -i "s|{{ TITLE }}|$TITLE|" "tmp/$FILENAME.html"
+  echo "  Checking file presence."
+  if [ ! -f $TITLE_FILE ]; then echo "No file $TITLE_FILE found.  Exiting."; exit; fi
+  if [ ! -f $OVERVIEW_FILE ]; then echo "No file $OVERVIEW_FILE found.  Exiting."; exit; fi
+  if [ ! -f $ARTICLE_FILE ]; then echo "No file $ARTICLE_FILE found.  Exiting."; exit; fi
+  if [ ! -f $COPYRIGHT_FILE ]; then echo "No file $COPYRIGHT_FILE found.  Exiting."; exit; fi
 
-  mv tmp/$FILENAME.html webroot/
+  echo "  Reading data."
+  TITLE=$( cat $TITLE_FILE )
+  ARTICLE=$( cat $ARTICLE_FILE )
+  OVERVIEW=$( cat $OVERVIEW_FILE )
+  COPYRIGHT=$( cat $COPYRIGHT_FILE )
+
+  echo "  Generating template."
+  cat layout/head.html    >  "tmp/$ID.html"
+  cat layout/article.html >> "tmp/$ID.html"
+  cat layout/foot.html    >> "tmp/$ID.html"
+
+  echo "  Populating template with data."
+  sed -i -e "s|{{ TITLE }}|$TITLE|" \
+         -e "s|{{ ID }}|$(echo $ID)|" \
+         -e "s|{{ COPYRIGHT }}|$(echo $COPYRIGHT)|" \
+         -e "s|{{ OVERVIEW }}|$(echo $OVERVIEW)|" \
+         -e "s|{{ ARTICLE }}|$(echo $ARTICLE)|" \
+         "tmp/$ID.html"
+
+  echo "  Moving into webroot."
+  mv "tmp/$ID.html" webroot/
+
+  ASSETS="articles/$ID/assets"
+  if [ -e $ASSETS ]
+  then
+
+    echo "  Copying over assets."
+    mkdir -p "webroot/assets/$ID"
+    cp -r $ASSETS/* "webroot/assets/$ID"
+
+  fi
 
 done
 
-rmdir tmp
 
-cp -r static webroot/
-cp -r assets webroot/
+
+echo "Building site pages."
+for page in pages/site/*
+do
+
+  echo "Operating on $page."
+
+  echo "  Generating asset filenames."
+  ID=$(echo $page | sed 's/[^/]*\/[^/]*\///' )
+  TITLE_FILE="pages/site/$ID/title"
+  BODY_FILE="pages/site/$ID/body"
+
+  echo "  Checking file presence."
+  if [ ! -f $TITLE_FILE ]; then echo "No file $TITLE_FILE found.  Exiting."; exit; fi
+  if [ ! -f $BODY_FILE ]; then echo "No file $BODY_FILE found.  Exiting."; exit; fi
+
+  echo "  Reading data."
+  TITLE=$( cat $TITLE_FILE )
+  BODY=$( cat $BODY_FILE )
+
+  echo "  Generating template."
+  cat layout/head.html >  "tmp/$ID.html"
+  cat $BODY_FILE       >> "tmp/$ID.html"
+  cat layout/foot.html >> "tmp/$ID.html"
+
+  echo "  Populating template with data."
+  sed -i -e "s|{{ TITLE }}|$TITLE|" "tmp/$ID.html"
+
+  echo "  Moving into webroot."
+  mv "tmp/$ID.html" webroot/
+
+done
+
+
+echo "Cleaning up."
+rmdir tmp
